78da5722534f74706e78ddcd9e718989bd9d921d
LuanPeixotop <luanpeixotopereira@gmail.com>
2021-11-12 12:52:41 -0300

    feat: adiciona a parte pwa do app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+
 import { fetchWeather } from './api/fetchWeather';
 import './App.css';
 
@@ -17,15 +18,7 @@ const App = () => {
 
   return (
     <div className="main-container">
-      <input
-        type="text"
-        className="search"
-        placeholder="Buscar..."
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-        onKeyPress={search}
-      />
-
+      <input type="text" className="search" placeholder="Search..." value={query} onChange={(e) => setQuery(e.target.value)} onKeyPress={search} />
       {weather.main && (
         <div className="city">
           <h2 className="city-name">
@@ -37,15 +30,11 @@ const App = () => {
             <sup>&deg;C</sup>
           </div>
           <div className="info">
-            <img
-              className="city-icon"
-              src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
-              alt={weather.weather[0].description}
-            />
+            <img className="city-icon" src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description} />
             <p>{weather.weather[0].description}</p>
           </div>
         </div>
-      )};
+      )}
     </div>
   );
 }
